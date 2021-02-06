af061a5942377d412d51e67e57a227851f6877b0
yooncastle <dbstjd427@gmail.com>
2021-02-07 02:22:34 +0900

    프로그래머스_lv2_20210207

--- myAPI.js
+++ myAPI.js
@@ -2,7 +2,7 @@
 const arr2 = Array.from(Array(9), () => Array(9).fill(0))
 arr2[0][0] = 2;
 
-showMatrix(arr2);
+//showMatrix(arr2);
 function showMatrix (arr) {
     arr.forEach((el) => {
         var temp = [];
@@ -34,3 +34,55 @@ for (var k = 2; k < 3; k++) {
 function gcd (a, b) {
     return a % b ? gcd(b, a % b) : b
 }
+
+
+//조합((1,2)===(2,1))
+const getCombinations = function (arr, selectNumber) {
+    const results = [];
+    if (selectNumber === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
+
+    arr.forEach((fixed, index, origin) => {
+        const rest = origin.slice(index + 1); // 해당하는 fixed를 제외한 나머지 뒤
+        const combinations = getCombinations(rest, selectNumber - 1); // 나머지에 대해서 조합을 구한다.
+        const attached = combinations.map((combination) => [fixed, ...combination]); //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
+        results.push(...attached); // 배열 spread syntax 로 모두다 push
+    });
+
+    return results; // 결과 담긴 results return
+}
+
+
+//순열((1,2)!==(2,1))
+const getPermutations = function (arr, selectNumber) {
+    const results = [];
+    if (selectNumber === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
+
+    arr.forEach((fixed, index, origin) => {
+        const rest = [...origin.slice(0, index), ...origin.slice(index + 1)] // 해당하는 fixed를 제외한 나머지 뒤
+        const permutations = getPermutations(rest, selectNumber - 1); // 나머지에 대해서 조합을 구한다.
+        const attached = permutations.map((permutations) => [fixed, ...permutations]); //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
+        results.push(...attached); // 배열 spread syntax 로 모두다 push
+    });
+
+    return results; // 결과 담긴 results return
+}
+
+
+//소수 찾기
+list.forEach((el, idx) => {
+    isPrime = true;
+
+    for (var l = 2; l <= Math.sqrt(el); l++) {
+        if (el % l == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (el === 1 || el === 0) {
+        isPrime = false;
+    }
+
+    if (isPrime) {
+        answer++;
+    }
+})

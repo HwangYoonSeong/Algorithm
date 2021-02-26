96eedfe1b48c294b65116eb6aae994bd939c1559
yooncastle <dbstjd427@gmail.com>
2021-02-26 17:38:55 +0900

    20210226

--- 프로그래머스/level2/오픈채팅방.js
+++ 프로그래머스/level2/오픈채팅방.js
@@ -1,4 +1,43 @@
 function solution (record) {
+    var answer = [];
+    var userList = [];
+
+
+    //1. 먼저 배열을 하나 생성해 2차원 배열로 split을 한다.
+    let newArr = record.map((str) => str.split(" "));
+
+    //2. 그 후 기록의 마지막 닉네임을 아이디와 매칭합니다.
+    //2.1 여기서 Enter와 Change 가 length가 3임을 이용하면 더 쉽게 접근할 수 있습니다.
+    let nickNameSet = {};
+    for (let i = 0; i < newArr.length; i++) {
+        if (newArr[i].length === 3) {
+            nickNameSet[newArr[i][1]] = newArr[i][2];
+        }
+    }
+
+
+    for (var j = 0; j < newArr.length; j++) {
+        switch (newArr[j][0]) {
+            case 'Enter':
+                answer.push(nickNameSet[newArr[j][1]] + "님이 들어왔습니다.");
+                break;
+
+            case 'Leave':
+                answer.push(nickNameSet[newArr[j][1]] + "님이 나갔습니다.");
+                break;
+
+
+        }
+    }
+
+    return answer;
+}
+
+console.log(solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"]))
+
+
+//모범 답안
+function solution2 (record) {
     let answer = [];
 
     //1. 먼저 배열을 하나 생성해 2차원 배열로 split을 한다.
@@ -24,5 +63,3 @@ function solution (record) {
 
     return answer;
 }
-
-console.log(solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"]))
---
+++ 프로그래머스/level3/섬연결하기.js
@@ -0,0 +1,113 @@
+// 4,7,8 런타임 에러 
+function solution2 (n, costs) {
+    var answer = 0;
+    var costsList = [];
+    for (var i = 0; i < costs.length; i++) {
+        costsList.push([i, costs[i][2]]);
+    }
+
+
+    //조합((1,2)===(2,1))
+    const getCombinations = function (arr, selectNumber) {
+        const results = [];
+        if (selectNumber === 1) return arr.map((value) => [value]); // 1개씩 택할 때, 바로 모든 배열의 원소 return
+
+        arr.forEach((fixed, index, origin) => {
+            const rest = origin.slice(index + 1); // 해당하는 fixed를 제외한 나머지 뒤
+            const combinations = getCombinations(rest, selectNumber - 1); // 나머지에 대해서 조합을 구한다.
+            const attached = combinations.map((combination) => [fixed, ...combination]); //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
+            results.push(...attached); // 배열 spread syntax 로 모두다 push
+        });
+
+        return results; // 결과 담긴 results return
+    }
+
+    //console.log(costsList);
+    var sumList = [];
+    var comb = getCombinations(costsList, n - 1)
+    //console.log(comb);
+    comb.sort((a, b) => {
+        return (a.reduce((acc, cur) => { return acc + cur[1] }, 0)) - (b.reduce((acc, cur) => { return acc + cur[1] }, 0))
+    }
+    )
+
+    //console.log(comb);
+
+
+    //bfs
+    function bfs (v, adj) {
+        var visited = new Array(n).fill(false);
+        var queue = [];
+        var temp = [];
+        queue.push(v);
+        while (queue.length) {
+            v = queue.shift();
+            if (visited[v] === false) {
+                visited[v] = true;
+                //console.log(v);
+                temp.push(v);
+                for (var i = 0; i < n; i++) {//n은 정점의 개수
+
+                    if (adj[v][i] && (visited[i] === false)) queue.push(i);
+                }
+            }
+        }
+
+        return temp.length;
+    }
+
+    for (var i = 0; i < comb.length; i++) {
+        var adj = Array.from(Array(n), () => Array(n).fill(0))
+        for (var j = 0; j < n - 1; j++) {
+            adj[costs[comb[i][j][0]][0]][costs[comb[i][j][0]][1]] = 1;
+            adj[costs[comb[i][j][0]][1]][costs[comb[i][j][0]][0]] = 1;
+        }
+
+        if (bfs(costs[comb[i][0][0]][0], adj) === n) {
+            answer = comb[i].reduce((a, b) => { return a + b[1] }, 0);
+            break
+        }
+
+
+    }
+    return answer;
+}
+
+// 모범 답안 (Union-Find)
+function solution (n, costs) {
+    var answer = 0;
+    costs.sort((a, b) => a[2] - b[2])
+    console.log(costs);
+    var cycleTable = Array(n).fill(0).map((element, index) => index)
+
+    while (!isOne(cycleTable)) {
+        var current = costs.shift()
+        var one = current[0]
+        var the_other = current[1]
+        var cost = current[2]
+        if (cycleTable[the_other] !== cycleTable[one]) {
+            cycleTable = changeCycleTableNumber(cycleTable, one, the_other)
+            answer += cost
+        }
+    }
+    return answer;
+}
+
+function changeCycleTableNumber (cycleTable, one, the_other) {
+    var cycleTheOther = cycleTable[the_other]
+    var cycleOne = cycleTable[one]
+    for (var i = 0; i < cycleTable.length; i++) {
+        if (cycleTable[i] === cycleTheOther) cycleTable[i] = cycleOne
+    }
+    return cycleTable
+}
+
+function isOne (cycleTable) {
+    for (var i = 1; i < cycleTable.length; i++) {
+        if (cycleTable[i - 1] !== cycleTable[i]) return false
+    }
+    return true
+}
+
+
+console.log(solution(4, [[0, 1, 1], [0, 2, 2], [1, 2, 5], [1, 3, 1], [2, 3, 8]]));

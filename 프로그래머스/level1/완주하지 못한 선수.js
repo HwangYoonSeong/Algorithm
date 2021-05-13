9a00deea53df1cc8f954e4aa8cef5a460af5b50e
yooncastle <dbstjd427@gmail.com>
2021-05-13 14:44:38 +0900

    20210513

--- 프로그래머스/level1/완주하지 못한 선수.js
+++ 프로그래머스/level1/완주하지 못한 선수.js
@@ -1,4 +1,4 @@
-function solution (participant, completion) {
+function solution1 (participant, completion) {
     var answer = '';
     participant.sort()
     completion.sort()
@@ -12,4 +12,20 @@ function solution (participant, completion) {
     }
 }
 
-console.log(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
+//hash로 푼 방법
+function solution2 (participant, completion) {
+    const hash = {};
+
+    for (let val of participant) {
+        if (!hash[val]) hash[val] = 0;
+        hash[val]++;
+    }
+    console.log(hash)
+
+    //완주한 선수는 제외
+    completion.forEach(val => hash[val]--);
+
+    //값이 남아있는 선수가 완주하지 못한 선수
+    for (let key in hash) if (hash[key]) return key;
+}
+console.log(solution2(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
